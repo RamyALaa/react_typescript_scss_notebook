d2b72f9c29ad384c2a8bc0d5ccb3bb821e228d7b
RamyALaa <ramy.aloocy@gmail.com>
2022-11-18 11:30:36 +0000

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-
+import {Routes, Route} from 'react-router-dom'
 import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>hi</h1>
+    <Routes>
+      <Route path='/' element={<h1>hi</h1>} />
+      <Route path='/new' element={<h1>new</h1>} />
+    </Routes>
   )
 }
 
