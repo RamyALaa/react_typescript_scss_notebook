faf19ab64019a22ee51b5cf29e02177e82709ff6
RamyALaa <ramy.aloocy@gmail.com>
2022-11-19 02:10:51 +0000

    init noteList

---
+++ src/NoteList.tsx
@@ -0,0 +1,10 @@
+import { RawNote, Tag } from "./App"
+
+type NoteListProps = {
+    notes : RawNote
+    tags : Tag[]
+}
+
+export function NoteList({notes, tags}:NoteListProps){
+    return ( <></>)
+}
